5beb865fa6eca6fde61d09a64b38d1ba9e20b5e8
jconstantine <jconstantine@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-10-13 21:44:45 +0000

    added a trivial test which uses the Spring/OSGi JUnit testing framework, but does so out side of the Spring/OSGi maven project so it doesn't inherit any special sauce
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@153 2e8e966d-6415-0410-94b9-dc4e65435d03

---
+++ out-of-project-test/src/test/java/org/example/simple/EmptyTest.java
@@ -0,0 +1,14 @@
+package org.example.simple;
+
+import org.springframework.osgi.test.ConfigurableBundleCreatorTests;
+
+/**
+ * @author Hal Hildebrand
+ *         Date: Oct 13, 2006
+ *         Time: 2:23:30 PM
+ */
+public class EmptyTest extends ConfigurableBundleCreatorTests {
+    public void testMe() {
+        assertTrue(true);
+    }
+}
