77b02090132fd5ab4d8a517a6c22f1c75571e840
jconstantine <jconstantine@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-10-18 16:31:45 +0000

    updated my tests to conform to new api

--- hal-test/src/test/java/org/example/simple/KfTest.java
+++ hal-test/src/test/java/org/example/simple/KfTest.java
@@ -20,13 +20,13 @@ public class KfTest extends ConfigurableBundleCreatorTests {
 
     protected String[] getBundleLocations() {
         return new String[]{
-                localMavenArtifact("aopalliance.osgi", "1.0-SNAPSHOT"),
-                localMavenArtifact("commons-collections.osgi", "3.2-SNAPSHOT"),
-                localMavenArtifact("spring-aop", "2.1-SNAPSHOT"),
-                localMavenArtifact("spring-context", "2.1-SNAPSHOT"),
-                localMavenArtifact("spring-beans", "2.1-SNAPSHOT"),
-                localMavenArtifact("spring-osgi-core", "1.0-SNAPSHOT"),
-                localMavenArtifact("spring-jmx", "2.1-SNAPSHOT")
+                localMavenArtifact("org.springframework.osgi", "aopalliance.osgi", "1.0-SNAPSHOT"),
+                localMavenArtifact("org.springframework.osgi", "commons-collections.osgi", "3.2-SNAPSHOT"),
+                localMavenArtifact("org.springframework.osgi", "spring-aop", "2.1-SNAPSHOT"),
+                localMavenArtifact("org.springframework.osgi", "spring-context", "2.1-SNAPSHOT"),
+                localMavenArtifact("org.springframework.osgi", "spring-beans", "2.1-SNAPSHOT"),
+                localMavenArtifact("org.springframework.osgi", "spring-osgi-core", "1.0-SNAPSHOT"),
+                localMavenArtifact("org.springframework.osgi", "spring-jmx", "2.1-SNAPSHOT")
         };
     }
 
