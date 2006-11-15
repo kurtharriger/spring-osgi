b0e1d1c47835fd8772431d855793a32ca54e803f
jconstantine <jconstantine@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-11-15 15:48:15 +0000

    repairing delete

---
+++ spring-osgi-mock/src/main/java/java/org/springframework/osgi/mock/MockServiceRegistration.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.osgi.mock;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Costin Leau
+ * 
+ */
+public class MockServiceRegistration implements ServiceRegistration {
+
+	private ServiceReference reference;
+
+	public MockServiceRegistration() {
+		this(null);
+	}
+
+	public MockServiceRegistration(Dictionary props) {
+		reference = new MockServiceReference(this);
+		setProperties(props);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.ServiceRegistration#getReference()
+	 */
+	public ServiceReference getReference() {
+		return reference;
+	}
+
+	public void setReference(ServiceReference reference) {
+		this.reference = reference;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.ServiceRegistration#setProperties(java.util.Dictionary)
+	 */
+	public void setProperties(Dictionary props) {
+		if (reference instanceof MockServiceReference)
+			((MockServiceReference) reference).setProperties(props);
+		else
+			throw new IllegalArgumentException("cannot update properties - service reference is not a "
+					+ MockServiceReference.class.getName());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.ServiceRegistration#unregister()
+	 */
+	public void unregister() {
+	}
+
+}
