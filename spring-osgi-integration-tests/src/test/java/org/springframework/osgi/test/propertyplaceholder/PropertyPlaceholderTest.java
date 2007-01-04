cf14092279e413932601076ac80e99e33a404e43
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-01-04 11:18:47 +0000

    + add spring-osgi-extender to make the namespace work (when no internet access is available)
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@474 2e8e966d-6415-0410-94b9-dc4e65435d03

--- spring-osgi-integration-tests/src/test/java/org/springframework/osgi/test/propertyplaceholder/PropertyPlaceholderTest.java
+++ spring-osgi-integration-tests/src/test/java/org/springframework/osgi/test/propertyplaceholder/PropertyPlaceholderTest.java
@@ -59,6 +59,7 @@ public class PropertyPlaceholderTest extends ConfigurableBundleCreatorTests {
 				localMavenArtifact("org.springframework.osgi", "spring-beans", "2.1-SNAPSHOT"),
 				localMavenArtifact("org.springframework.osgi", "spring-context", "2.1-SNAPSHOT"),
 				localMavenArtifact("org.springframework.osgi", "spring-osgi-core", "1.0-SNAPSHOT"),
+				localMavenArtifact("org.springframework.osgi", "spring-osgi-extender", "1.0-SNAPSHOT"),
 				// required by cm_all for logging
 				localMavenArtifact("org.knopflerfish.bundles", "log_all", "2.0.0"),
 				localMavenArtifact("org.knopflerfish.bundles", "cm_all", "2.0.0") };
