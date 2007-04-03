e90a919ebb22efdca76a4710038eb02972d63597
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-04-03 11:35:15 +0000

    updated tests
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@716 2e8e966d-6415-0410-94b9-dc4e65435d03

--- spring-osgi-integration-tests/src/test/java/org/springframework/osgi/test/cardinality0to1/Cardinality0to1Test.java
+++ spring-osgi-integration-tests/src/test/java/org/springframework/osgi/test/cardinality0to1/Cardinality0to1Test.java
@@ -8,74 +8,67 @@ import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
 import org.springframework.osgi.test.cardinality0to1.test.MyListener;
 
 /**
- * @author Hal Hildebrand
- *         Date: Dec 6, 2006
- *         Time: 6:04:42 PM
+ * @author Hal Hildebrand Date: Dec 6, 2006 Time: 6:04:42 PM
  */
 public class Cardinality0to1Test extends AbstractConfigurableBundleCreatorTests {
 
-    protected String getManifestLocation() {
-        //return "classpath:org/springframework/osgi/test/cardinality0to1/Cardinality0to1Test.MF";
-    	return null;
-    }
+	protected String getManifestLocation() {
+		// return
+		// "classpath:org/springframework/osgi/test/cardinality0to1/Cardinality0to1Test.MF";
+		return null;
+	}
 
+	protected String[] getBundles() {
+		return new String[] {
+				localMavenArtifact("org.springframework.osgi", "commons-collections.osgi", "3.2-SNAPSHOT"),
+				localMavenArtifact("org.springframework.osgi", "org.springframework.osgi.test.simple.service",
+					"1.0-SNAPSHOT"),
+				localMavenArtifact("org.springframework.osgi", "org.springframework.osgi.test.cardinality0to1",
+					"1.0-SNAPSHOT") };
+	}
 
-    protected String[] getBundles() {
-        return new String[]{
-                localMavenArtifact("org.springframework.osgi", "commons-collections.osgi", "3.2-SNAPSHOT"),
-                localMavenArtifact("org.springframework.osgi", "org.springframework.osgi.test.simple.service",
-                                   "1.0-SNAPSHOT"),
-                localMavenArtifact("org.springframework.osgi", "org.springframework.osgi.test.cardinality0to1",
-                                   "1.0-SNAPSHOT")
-        };
-    }
+	public void test0to1Cardinality() throws Exception {
+		waitOnContextCreation("org.springframework.osgi.test.simpleservice");
+		waitOnContextCreation("org.springframework.osgi.test.cardinality.0to1");
+		BundleContext bundleContext = getBundleContext();
 
+		Bundle simpleService2Bundle = bundleContext.installBundle(getLocator().locateArtifact(
+			"org.springframework.osgi", "org.springframework.osgi.test.simple.service2", "1.0-SNAPSHOT").getURL().toExternalForm());
 
-    public void test0to1Cardinality() throws Exception {
-        waitOnContextCreation("org.springframework.osgi.test.simpleservice");
-        waitOnContextCreation("org.springframework.osgi.test.cardinality.0to1");
-        BundleContext bundleContext = getBundleContext();
+		assertNotNull("Cannot find the simple service 2 bundle", simpleService2Bundle);
 
-        Bundle simpleService2Bundle = bundleContext.installBundle(
-                getLocator().locateArtifact("org.springframework.osgi", "org.springframework.osgi.test.simple.service2",
-                                   "1.0-SNAPSHOT").getURL().toExternalForm());
+		assertNotSame("simple service 2 bundle is in the activated state!", new Integer(Bundle.ACTIVE), new Integer(
+				simpleService2Bundle.getState()));
 
-        assertNotNull("Cannot find the simple service 2 bundle", simpleService2Bundle);
+		assertEquals("Unxpected initial binding of service", 0, MyListener.BOUND_COUNT);
+		assertEquals("Unexpected initial unbinding of service", 0, MyListener.UNBOUND_COUNT);
+		assertNotNull("Service reference should be not null", MyListener.service);
 
-        assertNotSame("simple service 2 bundle is in the activated state!",
-                      new Integer(Bundle.ACTIVE),
-                      new Integer(simpleService2Bundle.getState()));
+		try {
+			MyListener.service.stringValue();
+			fail("Service should be unavailable");
+		}
+		catch (ServiceUnavailableException e) {
+			// expected
+		}
 
+		startDependency(simpleService2Bundle);
 
-        assertEquals("Unxpected initial binding of service", 0, MyListener.BOUND_COUNT);
-        assertEquals("Unexpected initial unbinding of service", 0, MyListener.UNBOUND_COUNT);
-        assertNotNull("Service reference should be not null", MyListener.service);
+		assertEquals("Expected initial binding of service", 1, MyListener.BOUND_COUNT);
+		assertEquals("Unexpected initial unbinding of service", 0, MyListener.UNBOUND_COUNT);
+		assertNotNull("Service reference should be not null", MyListener.service);
 
-        try {
-            MyListener.service.stringValue();
-            fail("Service should be unavailable");
-        } catch (ServiceUnavailableException e) {
-            // expected
-        }
+		assertNotNull(MyListener.service.stringValue());
 
-        startDependency(simpleService2Bundle);
+	}
 
+	private void startDependency(Bundle simpleService2Bundle) throws BundleException, InterruptedException {
+		System.out.println("Starting dependency");
+		simpleService2Bundle.start();
 
-        assertEquals("Expected initial binding of service", 1, MyListener.BOUND_COUNT);
-        assertEquals("Unexpected initial unbinding of service", 0, MyListener.UNBOUND_COUNT);
-        assertNotNull("Service reference should be not null", MyListener.service);
+		waitOnContextCreation("org.springframework.osgi.test.simpleservice2");
 
-        assertNotNull(MyListener.service.stringValue());
-        
-    }
+		System.out.println("Dependency started");
+	}
 
-
-    private void startDependency(Bundle simpleService2Bundle) throws BundleException, InterruptedException {
-        System.out.println("Starting dependency");
-        simpleService2Bundle.start();
-
-        waitOnContextCreation("org.springframework.osgi.test.simpleservice2");
-
-        System.out.println("Dependency started");
-    }
 }
