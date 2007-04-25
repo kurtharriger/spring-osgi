ace28de95d49a29620c4d569f60acbcacf61b81d
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-04-25 10:22:07 +0000

    moved integration tests into their own package org.springframework.osgi.iandt

---
+++ spring-osgi-integration-tests/src/test/java/org/springframework/osgi/iandt/bundleScope/ScopingTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.osgi.iandt.bundleScope;
+
+import org.osgi.framework.ServiceReference;
+import org.springframework.osgi.iandt.scope.common.ScopeTestService;
+import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
+import org.springframework.osgi.util.OsgiFilterUtils;
+import org.springframework.osgi.util.OsgiServiceReferenceUtils;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * Integration tests for 'bundle' scoped beans.
+ * 
+ * @author Costin Leau
+ * 
+ */
+public class ScopingTest extends AbstractConfigurableBundleCreatorTests {
+
+	protected String[] getBundles() {
+		return new String[] {
+				localMavenArtifact("org.springframework.osgi", "cglib-nodep.osgi", "2.1.3-SNAPSHOT"),
+				localMavenArtifact("org.springframework.osgi", "org.springframework.osgi.iandt.scoped.bundle.common",
+					"1.0-m2-SNAPSHOT"),
+				localMavenArtifact("org.springframework.osgi", "org.springframework.osgi.iandt.scoped.bundle.a",
+					"1.0-m2-SNAPSHOT"),
+				localMavenArtifact("org.springframework.osgi", "org.springframework.osgi.iandt.scoped.bundle.b",
+					"1.0-m2-SNAPSHOT") };
+	}
+
+	protected String getManifestLocation() {
+		return "org/springframework/osgi/iandt/bundleScope/ScopingTest.MF";
+		// return null;
+	}
+
+	public void testEnvironmentValidity() throws Exception {
+		waitOnContextCreation("org.springframework.osgi.iandt.scope.a");
+		waitOnContextCreation("org.springframework.osgi.iandt.scope.b");
+		assertNotNull(getServiceA());
+		assertNotNull(getServiceB());
+	}
+
+	public void testServiceAScopeForCurrentBundle() throws Exception {
+		ScopeTestService serviceAcopy1 = getServiceA();
+		ScopeTestService serviceAcopy2 = getServiceA();
+
+		assertEquals("different bean instances given for the same bundle", serviceAcopy1, serviceAcopy2);
+	}
+
+	public void testServiceAScopeForBundleA() throws Exception {
+		ScopeTestService serviceAInBundleA = (ScopeTestService) org.springframework.osgi.iandt.scope.a.BeanReference.BEAN;
+
+		assertFalse("same bean instance used for different bundles",
+			serviceAInBundleA.equals(getServiceA().getServiceIdentity()));
+	}
+
+	public void testServiceAScopeForBundleB() throws Exception {
+		ScopeTestService serviceAInBundleB = (ScopeTestService) org.springframework.osgi.iandt.scope.b.BeanReference.BEAN;
+
+		assertFalse("same bean instance used for different bundles",
+			serviceAInBundleB.equals(getServiceA().getServiceIdentity()));
+	}
+
+	public void testServiceAScopeForBundleAAndBundleB() throws Exception {
+		ScopeTestService serviceAInBundleA = (ScopeTestService) org.springframework.osgi.iandt.scope.a.BeanReference.BEAN;
+		ScopeTestService serviceAInBundleB = (ScopeTestService) org.springframework.osgi.iandt.scope.b.BeanReference.BEAN;
+
+		assertFalse("same bean instance used for different bundles", serviceAInBundleA.getServiceIdentity().equals(
+			serviceAInBundleB.getServiceIdentity()));
+	}
+
+	protected ScopeTestService getServiceA() throws Exception {
+		return getService("a");
+	}
+
+	protected ScopeTestService getServiceB() throws Exception {
+		return getService("b");
+	}
+
+	protected ScopeTestService getService(String bundleName) {
+		ServiceReference ref = OsgiServiceReferenceUtils.getServiceReference(getBundleContext(),
+			ScopeTestService.class.getName(), "(Bundle-SymbolicName=org.springframework.osgi.iandt.scope." + bundleName
+					+ ")");
+		if (ref == null) {
+			String filter = OsgiFilterUtils.unifyFilter(ScopeTestService.class, null);
+			System.out.println(ObjectUtils.nullSafeToString(OsgiServiceReferenceUtils.getServiceReferences(getBundleContext(), filter)));
+			throw new IllegalStateException("cannot find service with owning bundle " + bundleName);
+		}
+		return (ScopeTestService) getBundleContext().getService(ref);
+
+	}
+}
---
+++ spring-osgi-integration-tests/src/test/java/org/springframework/osgi/iandt/propertyplaceholder/PropertyPlaceholderTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.osgi.iandt.propertyplaceholder;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.osgi.context.support.OsgiBundleXmlApplicationContext;
+import org.springframework.osgi.util.OsgiServiceUtils;
+import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
+
+/**
+ * 
+ * Integration test for OsgiPropertyPlaceholder.
+ * 
+ * @author Costin Leau
+ * 
+ */
+public class PropertyPlaceholderTest extends AbstractConfigurableBundleCreatorTests {
+
+	private final static String ID = "PropertyPlaceholderTest-123";
+
+	private final static Dictionary DICT = new Hashtable();
+
+	private ConfigurableApplicationContext ctx;
+
+	static {
+		DICT.put("foo", "bar");
+		DICT.put("white", "horse");
+		// This is needed when running under KF
+		System.setProperty("com.gatespace.bundle.cm.store", System.getProperty("user.dir"));
+	}
+
+	protected String getManifestLocation() {
+		return "classpath:org/springframework/osgi/iandt/propertyplaceholder/PropertyPlaceholder.MF";
+	}
+
+	protected String[] getBundles() {
+		return new String[] {
+				localMavenArtifact("org.springframework.osgi", "commons-collections.osgi", "3.2-SNAPSHOT"),
+				// required by cm_all for logging
+				localMavenArtifact("org.knopflerfish.bundles", "log_all", "2.0.0"),
+				localMavenArtifact("org.knopflerfish.bundles", "cm_all", "2.0.0") };
+	}
+
+	protected void onSetUp() throws Exception {
+		prepareConfiguration();
+
+		ctx = new OsgiBundleXmlApplicationContext(getBundleContext(),
+				new String[] { "org/springframework/osgi/iandt/propertyplaceholder/placeholder.xml" });
+		ctx.refresh();
+	}
+
+	protected void onTearDown() throws Exception {
+		if (ctx != null)
+			ctx.close();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.osgi.iandt.AbstractConfigurableBundleCreatorTests#getBundleContentPattern()
+	 */
+	protected String[] getBundleContentPattern() {
+		return super.getBundleContentPattern();
+	}
+
+	// add a default table into OSGi
+	private void prepareConfiguration() throws Exception {
+
+		ServiceReference ref = OsgiServiceUtils.getService(getBundleContext(), ConfigurationAdmin.class, null);
+
+		ConfigurationAdmin admin = (ConfigurationAdmin) getBundleContext().getService(ref);
+		Configuration config = admin.getConfiguration(ID);
+		config.update(DICT);
+	}
+
+	public void testFoundProperties() throws Exception {
+		String bean = (String) ctx.getBean("bean1");
+		assertEquals("horse", bean);
+	}
+
+	public void testFallbackProperties() throws Exception {
+		String bean = (String) ctx.getBean("bean2");
+		assertEquals("treasures", bean);
+	}
+}
