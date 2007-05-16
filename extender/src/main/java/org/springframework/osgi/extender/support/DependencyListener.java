37d4d6068cfbc0c6f281482cafe16526571b79e9
jconstantine <jconstantine@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-05-16 16:13:52 +0000

    Real fix for OSGI-125.  Log error message containing missing dependencies upon failure to create context
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@789 2e8e966d-6415-0410-94b9-dc4e65435d03

---
+++ extender/src/main/java/org/springframework/osgi/extender/support/DependencyListener.java
@@ -0,0 +1,166 @@
+package org.springframework.osgi.extender.support;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.osgi.service.importer.OsgiServiceProxyFactoryBean;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * ServiceListener used for tracking dependent services. As the
+ * ServiceListener receives event synchronously there is no need for
+ * synchronization.
+ *
+ * @author Costin Leau
+ * @author Hal Hildebrand
+ */
+public class DependencyListener implements ServiceListener {
+    protected final Set dependencies = new LinkedHashSet();
+    protected final Set unsatisfiedDependencies = new LinkedHashSet();
+    protected Runnable postAction;
+    protected ServiceDependentOsgiBundleXmlApplicationContext context;
+
+    private static final Log log = LogFactory.getLog(DependencyListener.class);
+
+
+    public DependencyListener(Runnable postAction, ServiceDependentOsgiBundleXmlApplicationContext context) {
+        this.postAction = postAction;
+        this.context = context;
+    }
+
+
+    protected void findServiceDependencies() {
+        ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+        String[] beans = BeanFactoryUtils.beanNamesForTypeIncludingAncestors(beanFactory,
+                                                                             OsgiServiceProxyFactoryBean.class,
+                                                                             true,
+                                                                             true);
+        for (int i = 0; i < beans.length; i++) {
+            String beanName = BeanFactory.FACTORY_BEAN_PREFIX + beans[i];
+            OsgiServiceProxyFactoryBean reference = (OsgiServiceProxyFactoryBean) beanFactory.getBean(beanName);
+            Dependency dependency = new Dependency(context.getBundleContext(), reference);
+            dependencies.add(dependency);
+            if (!dependency.isSatisfied()) {
+                unsatisfiedDependencies.add(dependency);
+            }
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug(dependencies.size() +
+                      " dependencies, " +
+                      unsatisfiedDependencies.size() +
+                      " unsatisfied for " +
+                      context.getDisplayName());
+        }
+
+    }
+
+
+    protected boolean isSatisfied() {
+        return unsatisfiedDependencies.isEmpty();
+    }
+    
+
+    public Set getUnsatisfiedDependencies() {
+        return unsatisfiedDependencies;
+    }
+
+
+    protected void register() {
+        boolean multiple = unsatisfiedDependencies.size() > 1;
+        StringBuffer sb = new StringBuffer(100 * unsatisfiedDependencies.size());
+        if (multiple) {
+            sb.append("(|");
+        }
+        for (Iterator i = unsatisfiedDependencies.iterator(); i.hasNext();) {
+            ((Dependency) i.next()).appendTo(sb);
+        }
+        if (multiple) {
+            sb.append(')');
+        }
+        String filter = sb.toString();
+        if (log.isInfoEnabled()) {
+            log.info(context.getDisplayName() + " registering listener with filter: " + filter);
+        }
+        try {
+            context.getBundleContext().addServiceListener(this, filter);
+        }
+        catch (InvalidSyntaxException e) {
+            throw (IllegalStateException) new IllegalStateException("Filter string '" + filter
+                                                                    + "' has invalid syntax: "
+                                                                    + e.getMessage()).initCause(e);
+        }
+    }
+
+
+    protected void deregister() {
+        try {
+            context.getBundleContext().removeServiceListener(this);
+        } catch (IllegalStateException e) {
+            // Bundle context is no longer valid
+        }
+    }
+
+
+    /**
+     * Process serviceChanged events, completing context initialization if
+     * all the required dependencies are satisfied.
+     *
+     * @param serviceEvent
+     */
+    public void serviceChanged(ServiceEvent serviceEvent) {
+        if (unsatisfiedDependencies.isEmpty()) { // already completed.
+            return;
+        }
+
+        for (Iterator i = dependencies.iterator(); i.hasNext();) {
+            Dependency dependency = (Dependency) i.next();
+            if (dependency.matches(serviceEvent)) {
+                switch (serviceEvent.getType()) {
+                    case ServiceEvent.MODIFIED:
+                    case ServiceEvent.REGISTERED:
+                        unsatisfiedDependencies.remove(dependency);
+                        if (log.isDebugEnabled()) {
+                            log.debug("found service; eliminating " + dependency);
+                        }
+                        break;
+                    case ServiceEvent.UNREGISTERING:
+                        unsatisfiedDependencies.add(dependency);
+                        if (log.isDebugEnabled()) {
+                            log.debug("service unregistered; adding " + dependency);
+                        }
+                        break;
+                    default: // do nothing
+                        break;
+                }
+            }
+        }
+
+        if (context.getState() == ContextState.INTERRUPTED || context.getState() == ContextState.CLOSED) {
+            deregister();
+            return;
+        }
+
+        // Good to go!
+        if (unsatisfiedDependencies.isEmpty()) {
+            deregister();
+            context.listener = null;
+            context.setState(ContextState.DEPENDENCIES_RESOLVED);
+            if (log.isDebugEnabled()) {
+                log.debug("No outstanding dependencies, completing initialization for "
+                          + context.getDisplayName());
+            }
+            postAction.run();
+        } else {
+            register(); // re-register with the new filter
+        }
+    }
+}
--- extender/src/main/java/org/springframework/osgi/extender/support/ServiceDependentOsgiBundleXmlApplicationContext.java
+++ extender/src/main/java/org/springframework/osgi/extender/support/ServiceDependentOsgiBundleXmlApplicationContext.java
@@ -16,19 +16,9 @@
  */
 package org.springframework.osgi.extender.support;
 
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.osgi.framework.*;
+import org.osgi.framework.BundleContext;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.BeanFactoryUtils;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.osgi.context.support.OsgiBundleXmlApplicationContext;
-import org.springframework.osgi.service.CardinalityOptions;
-import org.springframework.osgi.service.importer.OsgiServiceProxyFactoryBean;
-
-import java.util.*;
 
 /**
  * An OsgiBundleXmlApplicationContext which delays initialization until all
@@ -39,10 +29,8 @@ import java.util.*;
  * @author Costin Leau
  */
 public class ServiceDependentOsgiBundleXmlApplicationContext extends OsgiBundleXmlApplicationContext {
-    private volatile ContextState state = ContextState.INITIALIZED;
-    private DependencyListener listener;
-
-    private static final Log log = LogFactory.getLog(ServiceDependentOsgiBundleXmlApplicationContext.class);
+    protected volatile ContextState state = ContextState.INITIALIZED;
+    protected DependencyListener listener;
 
 
     public ServiceDependentOsgiBundleXmlApplicationContext(BundleContext context, String[] configLocations) {
@@ -51,7 +39,7 @@ public class ServiceDependentOsgiBundleXmlApplicationContext extends OsgiBundleX
 
     protected synchronized void create(final Runnable postAction) throws BeansException {
         preRefresh();
-        DependencyListener dl = new DependencyListener(postAction);
+        DependencyListener dl = new DependencyListener(postAction, this);
         dl.findServiceDependencies();
 
         state = ContextState.RESOLVING_DEPENDENCIES;
@@ -59,8 +47,8 @@ public class ServiceDependentOsgiBundleXmlApplicationContext extends OsgiBundleX
             listener = dl;
             listener.register();
         } else {
-            if (log.isDebugEnabled()) {
-                log.debug("No outstanding dependencies, completing initialization for "
+            if (logger.isDebugEnabled()) {
+                logger.debug("No outstanding dependencies, completing initialization for "
                           + getDisplayName());
             } 
             postAction.run();
@@ -89,198 +77,20 @@ public class ServiceDependentOsgiBundleXmlApplicationContext extends OsgiBundleX
 
 
     public boolean isAvailable() {
-        return isActive() && (state == ContextState.DEPENDENCIES_RESOLVED);
+        return isActive() && (state == ContextState.DEPENDENCIES_RESOLVED || state == ContextState.CREATED);
     }
 
 
-    /**
-     * ServiceListener used for tracking dependent services. As the
-     * ServiceListener receives event synchronously there is no need for
-     * synchronization.
-     *
-     * @author Costin Leau
-     */
-    protected class DependencyListener implements ServiceListener {
-        private final Set dependencies = new LinkedHashSet();
-        private final Set unsatisfiedDependencies = new LinkedHashSet();
-        private final Runnable postAction;
-
-
-        protected DependencyListener(Runnable postAction) {
-            this.postAction = postAction;
-        }
-
-        protected void findServiceDependencies() {
-            ConfigurableListableBeanFactory beanFactory = getBeanFactory();
-            String[] beans = BeanFactoryUtils.beanNamesForTypeIncludingAncestors(beanFactory,
-                                                                                 OsgiServiceProxyFactoryBean.class,
-                                                                                 true,
-                                                                                 true);
-            for (int i = 0; i < beans.length; i++) {
-                String beanName = BeanFactory.FACTORY_BEAN_PREFIX + beans[i];
-                OsgiServiceProxyFactoryBean reference = (OsgiServiceProxyFactoryBean) beanFactory.getBean(beanName);
-                Dependency dependency = new Dependency(reference);
-                dependencies.add(dependency);
-                if (!dependency.isSatisfied()) {
-                    unsatisfiedDependencies.add(dependency);
-                }
-            }
-
-            if (logger.isDebugEnabled()) {
-                logger.debug(dependencies.size() +
-                          " dependencies, " +
-                          unsatisfiedDependencies.size() +
-                          " unsatisfied for " +
-                          getDisplayName());
-            }
-
-        }
-
-
-        protected boolean isSatisfied() {
-            return unsatisfiedDependencies.isEmpty();
-        }
-
-
-        protected void register() {
-            boolean multiple = unsatisfiedDependencies.size() > 1;
-            StringBuffer sb = new StringBuffer(100 * unsatisfiedDependencies.size());
-            if (multiple) {
-                sb.append("(|");
-            }
-            for (Iterator i = unsatisfiedDependencies.iterator(); i.hasNext();) {
-                ((Dependency) i.next()).appendTo(sb);
-            }
-            if (multiple) {
-                sb.append(')');
-            }
-            String filter = sb.toString();
-            if (log.isInfoEnabled()) {
-                log.info(getDisplayName() + " registering listener with filter: " + filter);
-            }
-            try {
-                getBundleContext().addServiceListener(this, filter);
-            }
-            catch (InvalidSyntaxException e) {
-                throw (IllegalStateException) new IllegalStateException("Filter string '" + filter
-                                                                        + "' has invalid syntax: "
-                                                                        + e.getMessage()).initCause(e);
-            }
-        }
-
-        protected void deregister() {
-            try {
-                getBundleContext().removeServiceListener(this);
-            } catch (IllegalStateException e) {
-                // Bundle context is no longer valid
-            }
-        }
-
-
-        /**
-         * Process serviceChanged events, completing context initialization if
-         * all the required dependencies are satisfied.
-         *
-         * @param serviceEvent
-         */
-        public void serviceChanged(ServiceEvent serviceEvent) {
-            if (unsatisfiedDependencies.isEmpty()) { // already completed.
-                return;
-            }
-
-            for (Iterator i = dependencies.iterator(); i.hasNext();) {
-                Dependency dependency = (Dependency) i.next();
-                if (dependency.matches(serviceEvent)) {
-                    switch (serviceEvent.getType()) {
-                        case ServiceEvent.MODIFIED:
-                        case ServiceEvent.REGISTERED:
-                            unsatisfiedDependencies.remove(dependency);
-                            if (log.isDebugEnabled()) {
-                                log.debug("found service; eliminating " + dependency);
-                            }
-                            break;
-                        case ServiceEvent.UNREGISTERING:
-                            unsatisfiedDependencies.add(dependency);
-                            if (log.isDebugEnabled()) {
-                                log.debug("service unregistered; adding " + dependency);
-                            }
-                            break;
-                        default: // do nothing
-                            break;
-                    }
-                }
-            }
-
-            if (state == ContextState.INTERRUPTED || state == ContextState.CLOSED) {
-                deregister();
-                return;
-            }
-
-            // Good to go!
-            if (unsatisfiedDependencies.isEmpty()) {
-                deregister();
-                listener = null;
-                state = ContextState.DEPENDENCIES_RESOLVED;
-                if (log.isDebugEnabled()) {
-                    log.debug("No outstanding dependencies, completing initialization for "
-                              + getDisplayName());
-                }
-                postAction.run();
-            } else {
-                register(); // re-register with the new filter
-            }
-        }
+    public ContextState getState() {
+        return state;
     }
 
-    protected class Dependency {
-        private final String filterString;
-        private final Filter filter;
-        private final int cardinality;
-
-
-        private Dependency(OsgiServiceProxyFactoryBean reference) {
-            filter = reference.getUnifiedFilter();
-            filterString = filter.toString();
-            cardinality = reference.getCard();
-        }
-
-
-        public boolean matches(ServiceEvent event) {
-            return filter.match(event.getServiceReference());
-        }
-
-
-        public void appendTo(StringBuffer sb) {
-            sb.append(filterString);
-        }
-
-
-        public boolean isSatisfied() {
-            ServiceReference[] refs;
-            try {
-                refs = getBundleContext().getServiceReferences(null, filterString);
-            }
-            catch (InvalidSyntaxException e) {
-                throw (IllegalStateException) new IllegalStateException("Filter '" + filterString
-                                                                        + "' has invalid syntax: "
-                                                                        + e.getMessage()).initCause(e);
-            }
-            return !CardinalityOptions.atLeastOneRequired(cardinality) || (refs != null && refs.length != 0);
-        }
-
-
-        public String toString() {
-            return "Dependency on [" + filter + "]";
-        }
-
-
-        /**
-         * @return Returns the filter.
-         */
-        public Filter getFilter() {
-            return filter;
-        }
-
+    protected void setState(ContextState s) {
+        state = s;
     }
 
+
+    public DependencyListener getListener() {
+        return listener;
+    }
 }
