6b09cc906f425e588e47940ccdea13f3b5c0ce77
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-05-31 10:12:27 +0000

    + missing classes

---
+++ core/src/main/java/org/springframework/osgi/service/ServiceReferenceAware.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.osgi.service;
+
+import java.util.Map;
+
+import org.osgi.framework.ServiceReference;
+
+/**
+ * Aware interface used when importing OSGi services. Gives access to the
+ * underlying ServiceReference and its properties.
+ * 
+ * @author Costin Leau
+ * 
+ */
+public interface ServiceReferenceAware {
+
+	/**
+	 * Get access to the raw, underlying service reference.
+	 * 
+	 * @return underlying service reference.
+	 */
+	ServiceReference getServiceReference();
+
+	/**
+	 * Return a map of service properties.
+	 * 
+	 * @return
+	 */
+	Map getServiceProperties();
+
+}
