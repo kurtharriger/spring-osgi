89e5ebffcdc7aa9b5490d78a18d6b9fea92d7e78
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-08-19 14:16:14 +0000

    + moved all platform tests into one

---
+++ test-support/spring-osgi-test-integration/integration-tests/src/test/java/org/springframework/osgi/iandt/OsgiPlatformTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.osgi.iandt;
+
+import java.util.Properties;
+
+import org.osgi.framework.Constants;
+import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
+import org.springframework.osgi.test.platform.EquinoxPlatform;
+import org.springframework.osgi.test.platform.FelixPlatform;
+import org.springframework.osgi.test.platform.KnopflerfishPlatform;
+import org.springframework.osgi.test.platform.MBSProPlatform;
+import org.springframework.osgi.test.platform.OsgiPlatform;
+
+/**
+ * This test might log exceptions since the OSGi platform may try to register an
+ * URLStreamFactory every time they start (during {@link #setName(String)}).
+ * 
+ * Basically, this just reads the underlying platform and checks its vendor to
+ * make sure the mapping is correct.
+ * 
+ * @author Costin Leau
+ * 
+ */
+public class OsgiPlatformTest extends AbstractConfigurableBundleCreatorTests {
+
+	private OsgiPlatform platform;
+
+	protected void onSetUp() {
+		platform = createPlatform();
+
+	}
+
+	public void testGetConfigurationProperties() {
+		Properties configuration = platform.getConfigurationProperties();
+		assertNotNull(configuration);
+		assertFalse(configuration.isEmpty());
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.springframework.osgi.test.platform.EquinoxPlatform#start()}.
+	 */
+	public void testOsgiPlatform() throws Exception {
+
+		String vendor = getBundleContext().getProperty(Constants.FRAMEWORK_VENDOR);
+
+		if ("Eclipse".equals(vendor))
+			assertTrue(platform instanceof EquinoxPlatform);
+		if ("Apache Software Foundation".equals(vendor))
+			assertTrue(platform instanceof FelixPlatform);
+		if ("Knopflerfish".equals(vendor))
+			assertTrue(platform instanceof KnopflerfishPlatform);
+		if ("ProSyst".equals(vendor))
+			assertTrue(platform instanceof MBSProPlatform);
+	}
+}
