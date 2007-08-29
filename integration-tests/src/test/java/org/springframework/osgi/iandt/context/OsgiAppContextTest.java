9ab5c4fa7be497f205e03e77bfd3cc887d38ff75
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-08-29 14:16:27 +0000

    OSGI-182 test
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@997 2e8e966d-6415-0410-94b9-dc4e65435d03

---
+++ integration-tests/src/test/java/org/springframework/osgi/iandt/context/OsgiAppContextTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.osgi.iandt.context;
+
+import org.osgi.framework.BundleContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.osgi.context.ConfigurableOsgiBundleApplicationContext;
+import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
+
+/**
+ * Integration test on the functionality offered by OSGi app context.
+ * 
+ * @author Costin Leau
+ * 
+ */
+public class OsgiAppContextTest extends AbstractConfigurableBundleCreatorTests {
+
+	private BundleContext bundleContext;
+
+	public void testBundleContextAvailableAsBean() {
+		ApplicationContext ctx = applicationContext;
+		assertTrue("bundleContext not available as a bean",
+			applicationContext.containsBean(ConfigurableOsgiBundleApplicationContext.BUNDLE_CONTEXT_BEAN_NAME));
+	}
+
+	public void testBundleContextInjected() {
+		assertNotNull("bundleContext hasn't been injected into the test", bundleContext);
+	}
+
+	public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+	public void testBundleContextIsTheSame() {
+		assertSame(getBundleContext(), bundleContext);
+		assertSame(getBundleContext(),
+			applicationContext.getBean(ConfigurableOsgiBundleApplicationContext.BUNDLE_CONTEXT_BEAN_NAME));
+	}
+}
