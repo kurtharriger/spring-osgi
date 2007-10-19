649b81435bc8047968bf4661714713e623a7f45b
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-10-19 12:19:23 +0000

    OSGI-215
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@1108 2e8e966d-6415-0410-94b9-dc4e65435d03

--- core/src/main/java/org/springframework/osgi/internal/service/collection/comparator/OsgiServiceIdComparator.java
+++ core/src/main/java/org/springframework/osgi/internal/service/collection/comparator/OsgiServiceIdComparator.java
@@ -18,14 +18,26 @@ package org.springframework.osgi.internal.service.collection.comparator;
 import java.io.Serializable;
 import java.util.Comparator;
 
-import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
 import org.springframework.osgi.service.ServiceReferenceAware;
+import org.springframework.osgi.util.OsgiServiceReferenceUtils;
 
 /**
  * Default comparator for sorted collections. It uses the service id property of
  * an OSGi service to determine the order. Thus, by using this comparator, the
  * services added to a collection will be sorted in the order in which they are
- * published on the OSGi platform.
+ * published to the OSGi platform.
+ * 
+ * <p/> This comparator version, provides <i>natural</i> ordering for service
+ * references for pre OSGI 4.1 platforms, using the contract from OSGi 4.1 spec:
+ * 
+ * <blockquote> If this ServiceReference and the specified ServiceReference have
+ * the same service id they are equal. This ServiceReference is less than the
+ * specified ServiceReference if it has a lower service ranking and greater if
+ * it has a higher service ranking. Otherwise, if this ServiceReference and the
+ * specified ServiceReference have the same service ranking, this
+ * ServiceReference is less than the specified ServiceReference if it has a
+ * higher service id and greater if it has a lower service id. </blockquote>
  * 
  * @see Comparator
  * @author Costin Leau
@@ -34,18 +46,42 @@ import org.springframework.osgi.service.ServiceReferenceAware;
 public class OsgiServiceIdComparator implements Comparator, Serializable {
 
 	private static final int hashCode = OsgiServiceIdComparator.class.hashCode() * 13;
-	/*
-	 * (non-Javadoc)
-	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
-	 */
+
 	public int compare(Object o1, Object o2) {
-		ServiceReferenceAware obj1 = (ServiceReferenceAware) o1;
-		ServiceReferenceAware obj2 = (ServiceReferenceAware) o2;
 
-		Long id1 = (Long) obj1.getServiceProperties().get(Constants.SERVICE_ID);
-		Long id2 = (Long) obj2.getServiceProperties().get(Constants.SERVICE_ID);
+		ServiceReference ref1, ref2;
 
-		return id1.compareTo(id2);
+		// look first for service references
+		if (o1 instanceof ServiceReference && o2 instanceof ServiceReference) {
+			ref1 = (ServiceReference) o1;
+			ref2 = (ServiceReference) o2;
+		}
+		// then for ServiceReferenceAware objects
+		else {
+			ServiceReferenceAware obj1 = (ServiceReferenceAware) o1;
+			ServiceReferenceAware obj2 = (ServiceReferenceAware) o2;
+
+			ref1 = obj1.getServiceReference();
+			ref2 = obj2.getServiceReference();
+		}
+
+		return compare(ref1, ref2);
+	}
+
+	private int compare(ServiceReference ref1, ServiceReference ref2) {
+		long id1 = OsgiServiceReferenceUtils.getServiceId(ref1);
+		long id2 = OsgiServiceReferenceUtils.getServiceId(ref2);
+
+		if (id1 == id2)
+			return 0;
+
+		// compare based on service ranking
+		int rank1 = OsgiServiceReferenceUtils.getServiceRanking(ref1);
+		int rank2 = OsgiServiceReferenceUtils.getServiceRanking(ref2);
+
+		// when comparing IDs, make sure to return inverse results (i.e. lower
+		// id, means higher service)
+		return (rank1 < rank2 ? -1 : (rank1 == rank2) ? (id1 < id2 ? 1 : -1) : 1);
 	}
 
 	public boolean equals(Object obj) {
