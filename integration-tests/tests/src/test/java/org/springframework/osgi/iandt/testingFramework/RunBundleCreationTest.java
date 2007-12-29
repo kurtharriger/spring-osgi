f612bf524e1976574a7f74e922749542538e529f
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-12-29 15:33:07 +0000

    OSGi-290 + fix testing framework iandt
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@1245 2e8e966d-6415-0410-94b9-dc4e65435d03

--- integration-tests/tests/src/test/java/org/springframework/osgi/iandt/testingFramework/RunBundleCreationTest.java
+++ integration-tests/tests/src/test/java/org/springframework/osgi/iandt/testingFramework/RunBundleCreationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.osgi.iandt;
+package org.springframework.osgi.iandt.testingFramework;
 
 import junit.framework.TestCase;
 import junit.framework.TestResult;
