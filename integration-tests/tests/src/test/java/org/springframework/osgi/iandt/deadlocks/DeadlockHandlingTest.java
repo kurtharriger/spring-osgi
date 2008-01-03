3f124bfea26730cdc788c68e4b6d32c3f4768198
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-01-03 12:12:45 +0000

    + close service tracker on shutdown

--- integration-tests/tests/src/test/java/org/springframework/osgi/iandt/deadlocks/DeadlockHandlingTest.java
+++ integration-tests/tests/src/test/java/org/springframework/osgi/iandt/deadlocks/DeadlockHandlingTest.java
@@ -9,47 +9,46 @@ import org.springframework.osgi.context.ConfigurableOsgiBundleApplicationContext
 import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
 
 /**
- * @author Hal Hildebrand
- *         Date: Jun 5, 2007
- *         Time: 9:10:11 PM
+ * @author Hal Hildebrand Date: Jun 5, 2007 Time: 9:10:11 PM
  */
 public class DeadlockHandlingTest extends AbstractConfigurableBundleCreatorTests {
 
-    // Specifically do not wait
-    protected boolean shouldWaitForSpringBundlesContextCreation() {
-        return false;
-    }
+	// Specifically do not wait
+	protected boolean shouldWaitForSpringBundlesContextCreation() {
+		return false;
+	}
 
+	/**
+	 * While it may appear that this test is doing nothing, what it is doing is
+	 * testing what happens when the OSGi framework is shutdown while the
+	 * Spring/OSGi extender is deadlocked. If all goes well, the test will
+	 * gracefully end. If not, it will hang for quite a while.
+	 */
+	public void testErrorHandling() throws Exception {
+		Resource errorResource = getLocator().locateArtifact("org.springframework.osgi",
+			"org.springframework.osgi.iandt.deadlock", getSpringDMVersion());
+		assertNotNull("bundle resource exists", errorResource);
+		Bundle errorBundle = bundleContext.installBundle(errorResource.getURL().toExternalForm());
+		assertNotNull("bundle exists", errorBundle);
+		errorBundle.start();
+		StringBuffer filter = new StringBuffer();
 
-    /**
-     * While it may appear that this test is doing nothing, what it is doing is testing what
-     * happens when the OSGi framework is shutdown while the Spring/OSGi extender is deadlocked.
-     * If all goes well, the test will gracefully end.  If not, it will hang for quite a while.
-     */
-    public void testErrorHandling() throws Exception {
-        Resource errorResource = getLocator()
-                .locateArtifact("org.springframework.osgi", "org.springframework.osgi.iandt.deadlock",
-                                getSpringDMVersion());
-        assertNotNull("bundle resource exists", errorResource);
-        Bundle errorBundle = bundleContext.installBundle(errorResource.getURL().toExternalForm());
-        assertNotNull("bundle exists", errorBundle);
-        errorBundle.start();
-        StringBuffer filter = new StringBuffer();
+		filter.append("(&");
+		filter.append("(").append(Constants.OBJECTCLASS).append("=").append(
+			AbstractRefreshableApplicationContext.class.getName()).append(")");
+		filter.append("(").append(ConfigurableOsgiBundleApplicationContext.APPLICATION_CONTEXT_SERVICE_PROPERTY_NAME);
+		filter.append("=").append("org.springframework.osgi.iandt.deadlock").append(")");
+		filter.append(")");
+		ServiceTracker tracker = new ServiceTracker(bundleContext, bundleContext.createFilter(filter.toString()), null);
 
-        filter.append("(&");
-        filter.append("(").append(Constants.OBJECTCLASS).append("=")
-                .append(AbstractRefreshableApplicationContext.class.getName()).append(")");
-        filter.append("(")
-                .append(ConfigurableOsgiBundleApplicationContext.APPLICATION_CONTEXT_SERVICE_PROPERTY_NAME);
-        filter.append("=").append("org.springframework.osgi.iandt.deadlock").append(")");
-        filter.append(")");
-        ServiceTracker tracker = new ServiceTracker(bundleContext,
-                                                    bundleContext.createFilter(filter.toString()),
-                                                    null);
-        tracker.open();
+		try {
+			tracker.open();
 
-        AbstractRefreshableApplicationContext appContext =
-                (AbstractRefreshableApplicationContext) tracker.waitForService(3000);
-        assertNull("Deadlock context should not be published", appContext);
-    }
+			AbstractRefreshableApplicationContext appContext = (AbstractRefreshableApplicationContext) tracker.waitForService(3000);
+			assertNull("Deadlock context should not be published", appContext);
+		}
+		finally {
+			tracker.close();
+		}
+	}
 }
