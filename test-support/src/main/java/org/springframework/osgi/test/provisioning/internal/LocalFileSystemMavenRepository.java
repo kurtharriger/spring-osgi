38530e540e45dbc0a378e52afaed57cc1cb932a1
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-01-14 12:11:31 +0000

    OSGI-327 maven settings.xml localRepository is being picked up by the default artifact locator

--- test-support/src/main/java/org/springframework/osgi/test/provisioning/internal/LocalFileSystemMavenRepository.java
+++ test-support/src/main/java/org/springframework/osgi/test/provisioning/internal/LocalFileSystemMavenRepository.java
@@ -13,16 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.osgi.test.provisioning.internal;
 
 import java.io.File;
 import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.xml.DefaultDocumentLoader;
+import org.springframework.beans.factory.xml.DocumentLoader;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.osgi.test.provisioning.ArtifactLocator;
+import org.springframework.util.xml.DomUtils;
+import org.springframework.util.xml.XmlValidationModeDetector;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 /**
  * Locator for artifacts found in the local maven repository. Does <strong>not</strong>
@@ -36,6 +45,80 @@ import org.springframework.osgi.test.provisioning.ArtifactLocator;
 public class LocalFileSystemMavenRepository implements ArtifactLocator {
 
 	private static final Log log = LogFactory.getLog(LocalFileSystemMavenRepository.class);
+
+	/** local repo system property */
+	private static final String SYS_PROPERTY = "localRepository";
+	/** user home system property */
+	private static final String USER_HOME_PROPERTY = "user.home";
+	/** m2 local user settings */
+	private static final String M2_DIR = ".m2";
+	/** maven settings xml */
+	private static final String M2_SETTINGS = M2_DIR.concat("settings.xml");
+	/** default local repository */
+	private static final String DEFAULT_DIR = M2_DIR.concat("/repository");
+	/** discovered local m2 repository home */
+	private final String repositoryHome;
+
+
+	/**
+	 * Default constructor. Constructs a new
+	 * <code>LocalFileSystemMavenRepository</code> instance. This constructor
+	 * will also determine the repository path by checking the existence of
+	 * <code>localRepository</code> system property and falling back to the
+	 * <code>settings.xml</code> file and then the traditional
+	 * <code>user.home/.m2/repository</code>.
+	 */
+	public LocalFileSystemMavenRepository() {
+
+		boolean trace = log.isDebugEnabled();
+
+		// check system property
+		String localRepository = System.getProperty(SYS_PROPERTY);
+		if (trace)
+			log.trace("m2 sys property [" + SYS_PROPERTY + "] has value=" + localRepository);
+
+		if (localRepository == null) {
+			// if it's not present then check settings.xml local repository property
+			localRepository = getMavenSettingsLocalRepository(new FileSystemResource(new File(
+				System.getProperty(USER_HOME_PROPERTY), M2_SETTINGS)));
+			if (trace)
+				log.trace("falling back to M2 settings.xml file; found value=" + localRepository);
+			if (localRepository == null) {
+				// fall back to the default location
+				localRepository = new File(System.getProperty(USER_HOME_PROPERTY), DEFAULT_DIR).getAbsolutePath();
+				if (trace)
+					log.trace("no custom setting found; using defualt M2 local repository=" + localRepository);
+
+			}
+		}
+
+		repositoryHome = localRepository;
+		log.info("local Maven2 repository used: [" + repositoryHome + "]");
+	}
+
+	/**
+	 * Returns the <code>localRepository</code> settings as indicated by the
+	 * <code>settings.xml</code> file.
+	 * 
+	 * @return local repository as indicated by a Maven settings.xml file
+	 */
+	String getMavenSettingsLocalRepository(Resource m2Settings) {
+		// no file found, return null to continue the discovery process
+		if (!m2Settings.exists())
+			return null;
+
+		try {
+			DocumentLoader docLoader = new DefaultDocumentLoader();
+			Document document = docLoader.loadDocument(new InputSource(m2Settings.getInputStream()), null, null,
+				XmlValidationModeDetector.VALIDATION_NONE, false);
+
+			return (DomUtils.getChildElementValueByTagName(document.getDocumentElement(), "localRepository"));
+		}
+		catch (Exception ex) {
+			throw (RuntimeException) new RuntimeException(new ParserConfigurationException("error parsing resource="
+					+ m2Settings)).initCause(ex);
+		}
+	}
 
 	/**
 	 * Find a local maven artifact. First tries to find the resource as a
@@ -82,9 +165,6 @@ public class LocalFileSystemMavenRepository implements ArtifactLocator {
 	 * @return
 	 */
 	protected Resource localMavenBundle(String groupId, String artifact, String version, String type) {
-		String defaultHome = new File(new File(System.getProperty("user.home")), ".m2/repository").getAbsolutePath();
-		File repositoryHome = new File(System.getProperty("localRepository", defaultHome));
-
 		StringBuffer location = new StringBuffer(groupId.replace('.', '/'));
 		location.append('/');
 		location.append(artifact);
