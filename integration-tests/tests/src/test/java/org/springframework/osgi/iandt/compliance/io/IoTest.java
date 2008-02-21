482b448e6c6367e1643fd772129301431f654662
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-02-21 16:55:35 +0000

    OSGI-354 OSGI-210 OSGI-344 OSGI-315 + increased Felix required version to 1.0.3
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@1345 2e8e966d-6415-0410-94b9-dc4e65435d03

--- integration-tests/tests/src/test/java/org/springframework/osgi/iandt/compliance/io/IoTest.java
+++ integration-tests/tests/src/test/java/org/springframework/osgi/iandt/compliance/io/IoTest.java
@@ -20,6 +20,7 @@ import java.net.URL;
 import java.util.Enumeration;
 
 import org.springframework.osgi.iandt.io.BaseIoTest;
+import org.springframework.osgi.test.platform.Platforms;
 import org.springframework.util.ObjectUtils;
 
 /**
@@ -47,14 +48,14 @@ public class IoTest extends BaseIoTest {
 		assertNotNull(url);
 	}
 
-	// fails on Felix 1.0.1 (fixed in KF 2.0.3)
+	// fails on Felix 1.0.1 (fixed in 1.0.3 and KF 2.0.3)
 	public void testGetResourceOnRoot() throws Exception {
 		URL url = bundle.getResource("/");
 		System.out.println("getResource('/') = " + url);
 		assertNotNull("root folder not validated " + url);
 	}
 
-	// fails on Felix 1.0.1 (fixed in KF 2.0.3)
+	// fails on Felix 1.0.1 (fixed in 1.0.3 and KF 2.0.3)
 	public void testGetResourceSOnRoot() throws Exception {
 		Enumeration enm = bundle.getResources("/");
 		Object[] res = copyEnumeration(enm);
@@ -108,9 +109,5 @@ public class IoTest extends BaseIoTest {
 		assertTrue(enm.hasMoreElements());
 		assertTrue(((URL) enm.nextElement()).getPath().endsWith("/"));
 
-	}
-
-	protected boolean isDisabledInThisEnvironment(String testMethodName) {
-		return (isFelix() && (testMethodName.equalsIgnoreCase("testGetResourceOnRoot") || testMethodName.equalsIgnoreCase("testGetResourceSOnRoot")));
 	}
 }
--- test-support/src/main/java/org/springframework/osgi/test/AbstractDependencyManagerTests.java
+++ test-support/src/main/java/org/springframework/osgi/test/AbstractDependencyManagerTests.java
@@ -37,9 +37,9 @@ import org.springframework.util.StringUtils;
  * 
  * <p/>This implementation uses internally an {@link ArtifactLocator} to
  * retrieve the required dependencies for the running test. By default, the
- * artifact locator uses the local maven 2 repository. Currently, no Maven
- * configurations (such as &lt;settings.xml&gt;) are supported. If the maven
- * repository is located in a non-default location, use the
+ * artifact locator uses the local maven 2 repository. Maven configurations
+ * (such as &lt;settings.xml&gt;) are supported. Alternatively for Maven
+ * repositories located in non-default locations, one can use the
  * <code>localRepository</code> system property to specify the folder URL.
  * 
  * @author Costin Leau
@@ -256,7 +256,7 @@ public abstract class AbstractDependencyManagerTests extends AbstractSynchronize
 	/**
 	 * {@inheritDoc}
 	 * 
-	 * <p/>Sets specific log4j propety to avoid class loading problems during
+	 * <p/>Sets specific log4j property to avoid class loading problems during
 	 * start up related to the thread context class loader.
 	 */
 	protected void preProcessBundleContext(BundleContext platformBundleContext) throws Exception {
