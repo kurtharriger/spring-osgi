61842de172b67ca2abb5095fd010aec54d0a8ce2
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-03-07 16:52:06 +0000

    OSGI-397 + test case showing the fix + thanks go to Alexei Akimov for reporting the issue and creating an excellent test case + installed bundles have the location set to the URL (so that updates work)

---
+++ integration-tests/tests/src/test/java/org/springframework/osgi/iandt/proxycreator/ProxyCreatorTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2006-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.osgi.iandt.proxycreator;
+
+import org.osgi.framework.Bundle;
+import org.springframework.osgi.iandt.BaseIntegrationTest;
+import org.springframework.osgi.util.OsgiBundleUtils;
+
+/**
+ * @author Costin Leau
+ * 
+ */
+public class ProxyCreatorTest extends BaseIntegrationTest {
+
+	private static final String PROXY_CREATOR_SYM_NAME = "org.springframework.osgi.iandt.proxy.creator";
+
+
+	protected String[] getTestBundlesNames() {
+		return new String[] {
+			"org.springframework.osgi,org.springframework.osgi.iandt.proxy.creator," + getSpringDMVersion(),
+			"org.springframework.osgi,cglib-nodep.osgi,2.1.3-SNAPSHOT" };
+	}
+
+	public void testNewProxiesCreatedOnBundleRefresh() throws Exception {
+		// get a hold of the bundle proxy creator bundle and update it
+		Bundle bundle = OsgiBundleUtils.findBundleBySymbolicName(bundleContext, PROXY_CREATOR_SYM_NAME);
+
+		assertNotNull("proxy creator bundle not found", bundle);
+		// update bundle (and thus create a new version of the classes)
+		bundle.update();
+
+		// make sure it starts-up
+		try {
+			waitOnContextCreation(PROXY_CREATOR_SYM_NAME, 3);
+		}
+		catch (Exception ex) {
+			fail("updating the bundle failed");
+		}
+
+	}
+}
