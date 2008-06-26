7f781eeb2aa3d0e0811300e1d268c795ee5b4ef4
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-06-26 11:07:17 +0000

    OSGI-545 + removed deprecated exporter abstract class + updated changelog
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@1674 2e8e966d-6415-0410-94b9-dc4e65435d03

--- core/src/main/java/org/springframework/osgi/service/exporter/support/AbstractOsgiServiceExporter.java
+++ core/src/main/java/org/springframework/osgi/service/exporter/support/AbstractOsgiServiceExporter.java
@@ -27,10 +27,8 @@ import org.springframework.osgi.service.exporter.OsgiServiceRegistrationListener
  * known as exporting) Spring beans as OSGi services.
  * 
  * @author Costin Leau
- * @deprecated will be removed after 1.1 RC1 - use a concrete implementation
- * (such as OsgiServiceFactoryBean) instead.
  */
-public abstract class AbstractOsgiServiceExporter implements DisposableBean {
+abstract class AbstractOsgiServiceExporter implements DisposableBean {
 
 	/** listeners */
 	private OsgiServiceRegistrationListener[] listeners = new OsgiServiceRegistrationListener[0];
