8b27bd1184c07f7f10ad321eb7575e729ccda8a0
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-01-05 18:26:10 +0000

    OSGI-688 + consider non-osgi classloaders inside ChainedClassLoader

--- core/src/main/java/org/springframework/osgi/context/internal/classloader/ChainedClassLoader.java
+++ core/src/main/java/org/springframework/osgi/context/internal/classloader/ChainedClassLoader.java
@@ -24,6 +24,7 @@ import java.security.PrivilegedExceptionAction;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.osgi.framework.Bundle;
 import org.springframework.osgi.util.internal.ClassUtils;
 import org.springframework.util.Assert;
 
@@ -31,107 +32,238 @@ import org.springframework.util.Assert;
  * Chaining class loader implementation that delegates the resource and class
  * loading to a number of class loaders passed in.
  * 
+ * <p/> This class loader parent (by default the AppClassLoader) can be
+ * specified and will be added automatically as the last entry in the list.
+ * 
  * <p/> Additionally, the class space of this class loader can be extended at
- * runtime (by allowing more classloaders to be added).
+ * runtime (by allowing more class loaders to be added).
+ * 
+ * <strong>Note:</strong>non-OSGi class loaders are considered as special
+ * cases. As there are classes that are loaded by the Boot, Ext, App and Fwk
+ * ClassLoaders through boot delegation, this implementation tries to identify
+ * them and place them last in the chain. Otherwise, these loaders can pull in
+ * classes from outside OSGi causing {@link ClassCastException}s.
  * 
  * @author Costin Leau
  */
 public class ChainedClassLoader extends ClassLoader {
 
-	private final List loaders = new ArrayList();
-	/** index at which new classloader are added to */
-	private int index = 0;
+	/**
+	 * list of special class loaders, outside OSGi, that might be used by the
+	 * user through boot delegation
+	 */
+	private static final List knownNonOsgiLoaders = new ArrayList();
 
-	private final ClassLoader systemClassLoader;
+	// add the known, non-OSGi class loaders
+	// note that the order is important
+	static {
+		// start with the framework class loader
+		// then get all its parents (normally the this should be fwk -> (*) -> app -> ext -> boot)
+		// where (*) represents some optional loaders for cases where the framework is embedded
 
-
-	public ChainedClassLoader(ClassLoader[] loaders) {
-		Assert.notEmpty(loaders);
-		for (int i = 0; i < loaders.length; i++) {
-			ClassLoader classLoader = loaders[i];
-			Assert.notNull(classLoader, "null classloaders not allowed");
-			this.loaders.add(classLoader);
-		}
-
-		systemClassLoader = (ClassLoader) AccessController.doPrivileged(new PrivilegedAction() {
+		AccessController.doPrivileged(new PrivilegedAction() {
 
 			public Object run() {
-				return ClassLoader.getSystemClassLoader();
+
+				ClassLoader classLoader = getFwkClassLoader();
+				addNonOsgiClassLoader(classLoader);
+				// get the system class loader
+				classLoader = ClassLoader.getSystemClassLoader();
+				addNonOsgiClassLoader(classLoader);
+				return null;
 			}
 		});
 	}
 
-	public URL getResource(final String name) {
-		synchronized (loaders) {
-			if (System.getSecurityManager() != null) {
-				return (URL) AccessController.doPrivileged(new PrivilegedAction() {
+	/** list of loaders */
+	private final List loaders = new ArrayList();
 
-					public Object run() {
-						return doGetResource(name);
-					}
-				});
-			}
-			else {
-				return doGetResource(name);
+	/** list of special, non-osgi loaders, added by the user */
+	private final List nonOsgiLoaders = new ArrayList();
+
+	/** parent class loader */
+	private final ClassLoader parent;
+
+
+	/**
+	 * Constructs a new <code>ChainedClassLoader</code> instance.
+	 * 
+	 * Equivalent to {@link #ChainedClassLoader(ClassLoader[], ClassLoader)}
+	 * with the parent class loader initialized to the AppClassLoader
+	 * (practically the system bundle class loader).
+	 * 
+	 * Note that the AppClassLoader can be different then the
+	 * {@link #getSystemClassLoader()}, used by
+	 * {@link #ChainedClassLoader(ClassLoader[], ClassLoader)} if no parent is
+	 * specified.
+	 * 
+	 * @param loaders array of non-null class loaders
+	 */
+	public ChainedClassLoader(ClassLoader[] loaders) {
+		this(loaders, getFwkClassLoader());
+	}
+
+	/**
+	 * Constructs a new <code>ChainedClassLoader</code> instance.
+	 * 
+	 * @param loaders array of non-null class loaders
+	 * @param parent parent class loader (can be null)
+	 */
+	public ChainedClassLoader(ClassLoader[] loaders, ClassLoader parent) {
+		super(parent);
+
+		this.parent = parent;
+
+		Assert.notEmpty(loaders);
+
+		synchronized (this.loaders) {
+			for (int i = 0; i < loaders.length; i++) {
+				ClassLoader classLoader = loaders[i];
+				Assert.notNull(classLoader, "null classloaders not allowed");
+				addClassLoader(classLoader);
 			}
 		}
 	}
 
+	private static ClassLoader getFwkClassLoader() {
+		return (ClassLoader) AccessController.doPrivileged(new PrivilegedAction() {
+
+			public Object run() {
+				return Bundle.class.getClassLoader();
+			}
+		});
+	}
+
+	/**
+	 * Special static method used during the class initialization.
+	 * 
+	 * @param classLoader non OSGi class loader
+	 */
+	private static void addNonOsgiClassLoader(ClassLoader classLoader) {
+		while (classLoader != null) {
+			synchronized (knownNonOsgiLoaders) {
+				if (!knownNonOsgiLoaders.contains(classLoader)) {
+					knownNonOsgiLoaders.add(classLoader);
+				}
+			}
+			classLoader = classLoader.getParent();
+		}
+	}
+
+	public URL getResource(final String name) {
+		if (System.getSecurityManager() != null) {
+			return (URL) AccessController.doPrivileged(new PrivilegedAction() {
+
+				public Object run() {
+					return doGetResource(name);
+				}
+			});
+		}
+		else {
+			return doGetResource(name);
+		}
+	}
+
 	private URL doGetResource(String name) {
+		URL url = doGetResource(name, loaders);
+
+		if (url != null) {
+			return url;
+		}
+		else {
+			url = doGetResource(name, nonOsgiLoaders);
+		}
+
+		if (url != null) {
+			return url;
+		}
+
+		return (parent != null ? parent.getResource(name) : url);
+	}
+
+	private URL doGetResource(String name, List classLoaders) {
 		URL url = null;
-		for (int i = 0; i < loaders.size(); i++) {
-			ClassLoader loader = (ClassLoader) loaders.get(i);
-			url = loader.getResource(name);
-			if (url != null)
-				return url;
+		synchronized (classLoaders) {
+			for (int i = 0; i < classLoaders.size(); i++) {
+				ClassLoader loader = (ClassLoader) classLoaders.get(i);
+				url = loader.getResource(name);
+				if (url != null)
+					return url;
+			}
 		}
 		return url;
 	}
 
 	public Class loadClass(final String name) throws ClassNotFoundException {
 
-		synchronized (loaders) {
-			if (System.getSecurityManager() != null) {
-				try {
-					return (Class) AccessController.doPrivileged(new PrivilegedExceptionAction() {
+		if (System.getSecurityManager() != null) {
+			try {
+				return (Class) AccessController.doPrivileged(new PrivilegedExceptionAction() {
 
-						public Object run() throws Exception {
-							return doLoadClass(name);
-						}
-					});
-				}
-				catch (PrivilegedActionException pae) {
-					throw (ClassNotFoundException) pae.getException();
-				}
+					public Object run() throws Exception {
+						return doLoadClass(name);
+					}
+				});
 			}
-			else {
-				return doLoadClass(name);
+			catch (PrivilegedActionException pae) {
+				throw (ClassNotFoundException) pae.getException();
 			}
 		}
+		else {
+			return doLoadClass(name);
+		}
+	}
+
+	private Class doLoadClass(String name, List classLoaders) throws ClassNotFoundException {
+		Class clazz = null;
+
+		synchronized (classLoaders) {
+			for (int i = 0; i < classLoaders.size(); i++) {
+				ClassLoader loader = (ClassLoader) classLoaders.get(i);
+				try {
+					clazz = loader.loadClass(name);
+					return clazz;
+				}
+				catch (ClassNotFoundException e) {
+					// keep moving through the class loaders
+				}
+			}
+		}
+
+		return clazz;
 	}
 
 	private Class doLoadClass(String name) throws ClassNotFoundException {
-		Class clazz = null;
+		Class clazz = doLoadClass(name, loaders);
 
-		for (int i = 0; i < loaders.size(); i++) {
-			ClassLoader loader = (ClassLoader) loaders.get(i);
-			try {
-				clazz = loader.loadClass(name);
-				return clazz;
-			}
-			catch (ClassNotFoundException e) {
-				// keep moving through the class loaders
-			}
+		if (clazz != null) {
+			return clazz;
 		}
-		throw new ClassNotFoundException(name);
+		else {
+			clazz = doLoadClass(name, nonOsgiLoaders);
+		}
+
+		if (clazz != null) {
+			return clazz;
+		}
+
+		if (parent != null) {
+			return parent.loadClass(name);
+		}
+
+		else {
+			throw new ClassNotFoundException(name);
+		}
 	}
 
 	/**
-	 * Adds the classloader defining the given class.
+	 * Adds a class loader defining the given class, to the chained class loader
+	 * space.
 	 * 
 	 * @param clazz
 	 */
 	public void addClassLoader(final Class clazz) {
+		Assert.notNull(clazz, "a non-null class required");
 		addClassLoader((ClassLoader) AccessController.doPrivileged(new PrivilegedAction() {
 
 			public Object run() {
@@ -141,22 +273,58 @@ public class ChainedClassLoader extends ClassLoader {
 	}
 
 	/**
-	 * Adds the given classloader to the existing list. Note that since this
-	 * classloader is used internally for AOP purposes, the classloader will
-	 * always be added to
+	 * Adds the given class loader to the existing list.
 	 * 
-	 * @param classLoader
+	 * @param classLoader class loader to load classes from
 	 */
 	public void addClassLoader(ClassLoader classLoader) {
+		Assert.notNull(classLoader, "a non-null classLoader required");
+
+		if (!addNonOsgiLoader(classLoader)) {
+			addOsgiLoader(classLoader);
+		}
+	}
+
+	/**
+	 * Checks if the given classloader is a known, non-OSGi loader. If it is,
+	 * then it is added to a specific list based on the known ordering (ignoring
+	 * the user defined one). This is done so that the discovered hierarchy is
+	 * respected regardless of the user configuration.
+	 * 
+	 * @param classLoader
+	 * @return true if the class loader was added, false otherwise
+	 */
+	private boolean addNonOsgiLoader(ClassLoader classLoader) {
+		synchronized (nonOsgiLoaders) {
+			if (!nonOsgiLoaders.contains(classLoader)) {
+				synchronized (knownNonOsgiLoaders) {
+					int index = knownNonOsgiLoaders.indexOf(classLoader);
+					// add the class loader to the list if there is a match
+					if (index >= 0) {
+						int insertIndex = 0;
+						// but consider the defined order
+						for (int i = 0; i < nonOsgiLoaders.size(); i++) {
+							int presentLoaderIndex = knownNonOsgiLoaders.indexOf((ClassLoader) nonOsgiLoaders.get(i));
+							if (presentLoaderIndex >= 0 && presentLoaderIndex < index) {
+								insertIndex = i + 1;
+							}
+							else {
+								continue;
+							}
+						}
+						nonOsgiLoaders.add(insertIndex, classLoader);
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private void addOsgiLoader(ClassLoader classLoader) {
 		synchronized (loaders) {
 			if (!loaders.contains(classLoader)) {
-				// special case - add the system classloader last since otherwise you can run into CCE problems 
-				if (systemClassLoader.equals(classLoader)) {
-					loaders.add(classLoader);
-				}
-				else {
-					loaders.add(++index, classLoader);
-				}
+				loaders.add(classLoader);
 			}
 		}
 	}
