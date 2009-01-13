6ccf1269acee669463f38da177f880f8bba997cd
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-01-13 20:30:01 +0000

    OSGI-699 + upgrade to Felix 1.4.1 + improved FelixOsgiPlatform to support 1.0/1.2/1.4
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@1958 2e8e966d-6415-0410-94b9-dc4e65435d03

--- test-support/src/main/java/org/springframework/osgi/test/platform/FelixPlatform.java
+++ test-support/src/main/java/org/springframework/osgi/test/platform/FelixPlatform.java
@@ -17,28 +17,27 @@
 package org.springframework.osgi.test.platform;
 
 import java.io.File;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.felix.framework.Felix;
+import org.apache.felix.framework.util.StringMap;
+import org.apache.felix.main.AutoActivator;
 import org.apache.felix.main.Main;
 import org.osgi.framework.BundleContext;
 import org.springframework.osgi.test.internal.util.IOUtils;
-import org.springframework.util.ReflectionUtils;
 
 /**
- * Apache Felix (1.0.3+) OSGi platform.
+ * Apache Felix (1.0.3+/1.4.x+) OSGi platform.
  * 
  * @author Costin Leau
  */
 public class FelixPlatform extends AbstractOsgiPlatform {
-
-	private static final String BUNDLE_CONTEXT_METHOD = "getBundleContext";
-
-	private static final String FELIX_PRIVATE_FIELD = "m_felix";
 
 	private static final Log log = LogFactory.getLog(FelixPlatform.class);
 
@@ -63,7 +62,7 @@ public class FelixPlatform extends AbstractOsgiPlatform {
 		createStorageDir(props);
 		// disable logging
 		props.put("felix.log.level", "0");
-		
+
 		// use embedded mode
 		props.put("felix.embedded.execution", "true");
 		return props;
@@ -94,25 +93,76 @@ public class FelixPlatform extends AbstractOsgiPlatform {
 
 	public void start() throws Exception {
 		if (platform == null) {
-			// use Felix main and then read the felix instance
-
 			// initialize properties and set them as system wide so Felix can pick them up
 			System.getProperties().putAll(getPlatformProperties());
 
-			Main.main(new String[0]);
-
-			// read the Felix private field
-			Field field = Main.class.getDeclaredField(FELIX_PRIVATE_FIELD);
-			ReflectionUtils.makeAccessible(field);
-			platform = (Felix) field.get(Main.class);
-
-			// call getBundleContext
-			final Method getContext = platform.getClass().getDeclaredMethod(BUNDLE_CONTEXT_METHOD, null);
-
-			ReflectionUtils.makeAccessible(getContext);
-
-			context = (BundleContext) getContext.invoke(platform, null);
+			platform = configureFelix();
+			platform.start();
+			context = platform.getBundleContext();
 		}
+	}
+
+	/**
+	 * Configures the embedded Felix instance. This facade-like method is needed
+	 * since Felix 1.4.x breaks backwards compatibility by defining different
+	 * constructors.
+	 * 
+	 * @return a configured (but not started) Felix instance
+	 */
+	private Felix configureFelix() throws Exception {
+		boolean is14 = false;
+		Constructor ctr = null;
+		// check for Felix 1.4.x constructor
+		try {
+			ctr = Felix.class.getConstructor(new Class[] { Map.class });
+			is14 = true;
+		}
+		catch (NoSuchMethodException nsme) {
+			ctr = Felix.class.getConstructor(new Class[] { Map.class, List.class });
+		}
+
+		Object[] params = commonFelixSetup();
+		return (is14 ? configureFelix14X(ctr, params) : configureFelix10X_12X(ctr, params));
+	}
+
+	private Felix configureFelix10X_12X(Constructor ctr, Object[] params) throws Exception {
+		// Create a case-insensitive property map
+		Map configMap = new StringMap((Map) params[0], false);
+		return (Felix) ctr.newInstance(new Object[] { configMap, params[1] });
+	}
+
+	private Felix configureFelix14X(Constructor ctr, Object[] params) throws Exception {
+		Map configProps = (Map) params[0];
+		configProps.put("felix.systembundle.activators", params[1]);
+		return (Felix) ctr.newInstance(new Object[] { configProps });
+	}
+
+	/**
+	 * Runs the common 1.0.x/1.2.x/1.4.x Felix setup and returns two Felix
+	 * specific objects, the configuration properties and the list of
+	 * AutoActivators.
+	 * 
+	 * @return a 2 object array containing the configuration properties and the
+	 *         list of auto-activators
+	 */
+	private Object[] commonFelixSetup() {
+		// Load system properties.
+		Main.loadSystemProperties();
+
+		// Read configuration properties.
+		Properties configProps = Main.loadConfigProperties();
+
+		if (configProps == null) {
+			configProps = new Properties();
+		}
+
+		// Copy framework properties from the system properties.
+		Main.copySystemProperties(configProps);
+
+		List list = new ArrayList(1);
+		list.add(new AutoActivator(configProps));
+
+		return new Object[] { configProps, list };
 	}
 
 	public void stop() throws Exception {
