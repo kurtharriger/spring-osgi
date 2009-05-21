873045cabe1ceac79d19248fabc35f46f379c565
cleau <cleau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-05-21 17:59:42 +0000

    + aggregate integration testing (as well)
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@2157 2e8e966d-6415-0410-94b9-dc4e65435d03

---
+++ integration-tests/tests/src/test/java/org/springframework/osgi/iandt/blueprint/BaseBlueprintIntegrationTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2006-2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.osgi.iandt.blueprint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.osgi.iandt.BaseIntegrationTest;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * Blueprint base bundle. Contains RFC 124 specific items.
+ * 
+ * @author Costin Leau
+ * 
+ */
+public abstract class BaseBlueprintIntegrationTest extends BaseIntegrationTest {
+
+	protected String[] getTestFrameworkBundlesNames() {
+		String[] bundles = super.getTestFrameworkBundlesNames();
+		String[] matches = new String[] { "4j", "asm", "osgi-test", "junit" };
+
+		List<String> list = new ArrayList<String>(bundles.length);
+		for (int i = 0; i < bundles.length; i++) {
+			String bundle = bundles[i];
+//			for (int j = 0; j < matches.length; j++) {
+//				String match = matches[j];
+//				if (bundle.indexOf(match) > -1) {
+					list.add(bundle);
+//				}
+//			}
+		}
+		System.out.println(ObjectUtils.nullSafeToString(bundles));
+
+		// install event admin
+		list.add("org.apache.felix, org.apache.felix.eventadmin, 1.0.0");
+
+		// install the rfc 124 big bundle
+		list.add("org.springframework.osgi.rfc124,api," + getSpringDMVersion());
+		// list.add("org.springframework.osgi.rfc124.ri,all," + getSpringDMVersion());
+
+		System.out.println(list);
+		return list.toArray(new String[list.size()]);
+	}
+
+	protected String[] getBundleContentPattern() {
+		String pkg = getClass().getPackage().getName().replace('.', '/').concat("/");
+		String[] patterns = new String[] { BaseIntegrationTest.class.getName().replace('.', '/').concat("*.class"),
+				BaseBlueprintIntegrationTest.class.getName().replace('.', '/').concat("*.class"), pkg + "**/*" };
+		return patterns;
+	}
+}
---
+++ integration-tests/tests/src/test/java/org/springframework/osgi/iandt/blueprint/extender/ExtenderBootstrapTest.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2006-2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.osgi.iandt.blueprint.extender;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.service.blueprint.container.BlueprintContainer;
+import org.osgi.service.blueprint.container.BlueprintContainerListener;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+import org.springframework.core.io.Resource;
+import org.springframework.osgi.iandt.blueprint.BaseBlueprintIntegrationTest;
+import org.springframework.osgi.util.OsgiServiceReferenceUtils;
+import org.springframework.osgi.util.OsgiStringUtils;
+
+/**
+ * Integration test that checks basic behaviour signs of the RFC 124 Extender.
+ * 
+ * @author Costin Leau
+ * 
+ */
+public class ExtenderBootstrapTest extends BaseBlueprintIntegrationTest {
+
+	private Bundle testBundle;
+	private final Object monitor = new Object();
+
+	@Override
+	protected void onSetUp() throws Exception {
+		super.onSetUp();
+		installTestBundle();
+	}
+
+	@Override
+	protected void onTearDown() throws Exception {
+		if (testBundle != null) {
+			testBundle.uninstall();
+		}
+	}
+
+	public void testSanity() throws Exception {
+
+		EventHandler handler = new EventHandler() {
+
+			public void handleEvent(Event event) {
+				StringBuilder builder = new StringBuilder();
+				builder.append("Received event ").append(event).append(" w/ properties \n");
+
+				String[] propNames = event.getPropertyNames();
+
+				for (String propName : propNames) {
+					builder.append(propName).append("=").append(event.getProperty(propName)).append("\n");
+				}
+
+				System.out.println(builder.toString());
+			}
+		};
+
+		String[] topics = new String[] { "org/osgi/service/*" };
+		Dictionary<String, Object> prop = new Hashtable<String, Object>();
+		prop.put(EventConstants.EVENT_TOPIC, topics);
+		bundleContext.registerService(EventHandler.class.getName(), handler, prop);
+
+		testBundle.start();
+		Thread.sleep(1000 * 3);
+		testBundle.stop();
+		Thread.sleep(1000 * 3);
+	}
+
+	public void tstModuleContextService() throws Exception {
+		installTestBundle();
+		final boolean[] receviedEvent = new boolean[1];
+		ServiceListener notifier = new ServiceListener() {
+
+			public void serviceChanged(ServiceEvent event) {
+				// ModuleContext.SYMBOLIC_NAME_PROPERTY was removed - need to find a replacement
+				if (event.getServiceReference().getProperty(Constants.BUNDLE_SYMBOLICNAME) != null) {
+					logger.info("Found service "
+							+ OsgiServiceReferenceUtils.getServicePropertiesSnapshotAsMap(event.getServiceReference()));
+
+					synchronized (monitor) {
+						receviedEvent[0] = true;
+						monitor.notify();
+					}
+				}
+			}
+		};
+		// wait for the service up to 3 minutes
+		long waitTime = 2 * 60 * 1000;
+		bundleContext.addServiceListener(notifier);
+
+		testBundle.start();
+
+		synchronized (monitor) {
+			monitor.wait(waitTime);
+			assertTrue(receviedEvent[0]);
+		}
+		bundleContext.removeServiceListener(notifier);
+
+		testBundle.stop();
+		assertNull("module context service should be unpublished", bundleContext
+				.getServiceReference(BlueprintContainer.class.getName()));
+	}
+
+	public void testModuleContextListener() throws Exception {
+		final List<Bundle> contexts = new ArrayList<Bundle>();
+		BlueprintContainerListener listener = new BlueprintContainerListener() {
+
+			public void contextCreated(Bundle bundle) {
+				addToList(bundle);
+			}
+
+			public void contextCreationFailed(Bundle bundle, Throwable ex) {
+				addToList(bundle);
+			}
+
+			private void addToList(Bundle bundle) {
+				synchronized (contexts) {
+					contexts.add(bundle);
+					contexts.notify();
+				}
+			}
+		};
+
+		installTestBundle();
+		bundleContext.registerService(BlueprintContainerListener.class.getName(), listener, null);
+
+		testBundle.start();
+		synchronized (contexts) {
+			contexts.wait(2 * 1000 * 60);
+			assertFalse("no event received", contexts.isEmpty());
+		}
+	}
+
+	public void testLogInstalledBundles() throws Exception {
+		for (Bundle bundle : bundleContext.getBundles()) {
+			System.out.println("[" + bundle.getBundleId() + "] " + OsgiStringUtils.bundleStateAsString(bundle) + " "
+					+ OsgiStringUtils.nullSafeName(bundle) + "|" + bundle.getSymbolicName() + " @ "
+					+ bundle.getLocation());
+		}
+	}
+
+	private void installTestBundle() throws Exception {
+		Resource bundleResource = getLocator().locateArtifact("org.springframework.osgi.iandt.blueprint", "simple.bundle",
+				getSpringDMVersion());
+		testBundle = bundleContext.installBundle(bundleResource.getDescription(), bundleResource.getInputStream());
+	}
+}
