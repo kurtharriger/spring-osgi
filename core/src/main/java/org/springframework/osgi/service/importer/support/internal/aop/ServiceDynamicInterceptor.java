d511af38a993346f9d28e35f2147fe65a491664e
cleau <cleau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-06-12 15:53:02 +0000

    + improved laziness of the backing service retrieval
    git-svn-id: https://src.springframework.org/svn/spring-osgi/trunk@2201 2e8e966d-6415-0410-94b9-dc4e65435d03

--- core/src/main/java/org/springframework/osgi/service/importer/support/internal/aop/ServiceDynamicInterceptor.java
+++ core/src/main/java/org/springframework/osgi/service/importer/support/internal/aop/ServiceDynamicInterceptor.java
@@ -40,13 +40,11 @@ import org.springframework.osgi.service.importer.ServiceProxyDestroyedException;
 import org.springframework.osgi.service.importer.event.OsgiServiceDependencyWaitEndedEvent;
 import org.springframework.osgi.service.importer.event.OsgiServiceDependencyWaitStartingEvent;
 import org.springframework.osgi.service.importer.event.OsgiServiceDependencyWaitTimedOutEvent;
-import org.springframework.osgi.service.importer.support.OsgiServiceProxyFactoryBean;
 import org.springframework.osgi.service.importer.support.internal.dependency.ImporterStateListener;
 import org.springframework.osgi.service.importer.support.internal.exception.BlueprintExceptionFactory;
 import org.springframework.osgi.service.importer.support.internal.support.DefaultRetryCallback;
 import org.springframework.osgi.service.importer.support.internal.support.RetryCallback;
 import org.springframework.osgi.service.importer.support.internal.support.RetryTemplate;
-import org.springframework.osgi.service.importer.support.internal.support.ServiceWrapper;
 import org.springframework.osgi.service.importer.support.internal.util.OsgiServiceBindingUtils;
 import org.springframework.osgi.util.OsgiListenerUtils;
 import org.springframework.osgi.util.OsgiServiceReferenceUtils;
@@ -56,7 +54,9 @@ import org.springframework.util.ObjectUtils;
 /**
  * Interceptor adding dynamic behaviour for unary service (..1 cardinality). It will look for a service using the given
  * filter, retrying if the service is down or unavailable. Will dynamically rebound a new service, if one is available
- * with a higher service ranking. <p/> <p/> In case no service is available, it will throw an exception.
+ * with a higher service ranking and the interceptor is non sticky.
+ * 
+ * <p/> In case no service is available, it will throw an exception.
  * 
  * <p/> <strong>Note</strong>: this is a stateful interceptor and should not be shared.
  * 
@@ -103,7 +103,7 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 				throw new ServiceProxyDestroyedException();
 			}
 
-			return (wrapper != null) ? wrapper.getService() : null;
+			return (holder != null ? holder.getService() : null);
 		}
 	}
 
@@ -124,12 +124,6 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 				Thread.currentThread().setContextClassLoader(classLoader);
 				ServiceReference ref = event.getServiceReference();
 
-				// service id
-				long serviceId = ((Long) ref.getProperty(Constants.SERVICE_ID)).longValue();
-				// service ranking
-				Integer rank = (Integer) ref.getProperty(Constants.SERVICE_RANKING);
-				int ranking = (rank == null ? 0 : rank.intValue());
-
 				boolean debug = log.isDebugEnabled();
 				boolean publicDebug = PUBLIC_LOGGER.isDebugEnabled();
 
@@ -139,16 +133,12 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 					// same as ServiceEvent.REGISTERED
 				case (ServiceEvent.MODIFIED): {
 					// flag indicating if the service is bound or rebound
-					boolean servicePresent = false;
+					boolean servicePresent = (holder != null);
 
-					synchronized (lock) {
-						servicePresent = (wrapper != null && wrapper.isServiceAlive());
-					}
-
-					if (updateWrapperIfNecessary(ref, serviceId, ranking)) {
+					if (updateWrapperIfNecessary(ref)) {
 						// inform listeners
 						OsgiServiceBindingUtils.callListenersBind(bundleContext, proxy, ref, listeners);
-
+						// we have a bind
 						if (!servicePresent) {
 							notifySatisfiedStateListeners();
 						}
@@ -159,33 +149,23 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 				case (ServiceEvent.UNREGISTERING): {
 
 					boolean serviceRemoved = false;
-					/**
-					 * used if the service goes down and there is no replacement
-					 */
-					/**
-					 * since the listeners will require a valid proxy, the invalidation has to happen *after* calling
-					 * the listeners
-					 */
-					ServiceWrapper oldWrapper = wrapper;
+					//
+					// used if the service goes down and there is no replacement
+					//
+					// since the listeners will require a valid proxy, the invalidation has to happen *after* calling
+					// the listeners
+					//
+					ReferenceHolder oldHolder = holder;
 
-					synchronized (lock) {
-						// remove service
-						if (wrapper != null) {
-							if (serviceId == wrapper.getServiceId()) {
-								serviceRemoved = true;
-								wrapper = null;
-
-							}
-						}
+					// remove service
+					if (holder != null && holder.equals(ref)) {
+						serviceRemoved = true;
+						holder = null;
 					}
 
 					ServiceReference newReference = null;
 
-					boolean isDestroyed = false;
-
-					synchronized (lock) {
-						isDestroyed = destroyed;
-					}
+					boolean isDestroyed = destroyed;
 
 					// discover a new reference only if we are still running
 					if (!isDestroyed) {
@@ -204,22 +184,15 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 					}
 
 					// if no new reference was found and the service was indeed removed (it was bound to the
-					// interceptor)
-					// then do an unbind
+					// interceptor) then do an unbind
 					if (newReference == null && serviceRemoved) {
-
-						// reuse the old service for the time being
-						synchronized (lock) {
-							wrapper = oldWrapper;
-						}
+						// reuse the old service until the listeners are notified
+						holder = oldHolder;
 
 						// inform listeners
 						OsgiServiceBindingUtils.callListenersUnbind(bundleContext, proxy, ref, listeners);
 
-						// clean up wrapper
-						synchronized (lock) {
-							wrapper = null;
-						}
+						holder = null;
 
 						if (debug || publicDebug) {
 							String message = "Service reference [" + ref + "] was unregistered";
@@ -270,34 +243,17 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 			}
 		}
 
-		private boolean updateWrapperIfNecessary(ServiceReference ref, long serviceId, int serviceRanking) {
+		private boolean updateWrapperIfNecessary(ServiceReference ref) {
 			boolean updated = false;
 			try {
-				synchronized (lock) {
-					if (wrapper != null && wrapper.isServiceAlive()) {
-						// if there is a higher rank service
-						if (serviceRanking > wrapper.getServiceRanking()) {
-							updated = true;
-							updateReferenceHolders(ref);
-						}
-						// if equality, use the service id
-						if (serviceRanking == wrapper.getServiceRanking()) {
-							if (serviceId < wrapper.getServiceId()) {
-								updated = true;
-								updateReferenceHolders(ref);
-							}
-						}
-					}
-					// we don't have any valid services bounded yet so just bind
-					// the new
-					// one
-					else {
-						updated = true;
-						updateReferenceHolders(ref);
-					}
-					lock.notifyAll();
-					return updated;
+				if (holder == null || holder.isWorseThen(ref)) {
+					updated = true;
+					updateReferenceHolders(ref);
 				}
+				synchronized (lock) {
+					lock.notifyAll();
+				}
+				return updated;
 			} finally {
 				boolean debug = log.isDebugEnabled();
 				boolean publicDebug = PUBLIC_LOGGER.isDebugEnabled();
@@ -322,8 +278,7 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 		 * @param ref
 		 */
 		private void updateReferenceHolders(ServiceReference ref) {
-			// no need for a lock since this method is called from a synchronized block
-			wrapper = new ServiceWrapper(ref, bundleContext);
+			holder = new ReferenceHolder(ref, bundleContext);
 			referenceDelegate.swapDelegates(ref);
 		}
 	}
@@ -355,7 +310,7 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 	private boolean isDuringDestruction = false;
 
 	/** flag indicating whether the proxy is already destroyed or not */
-	private boolean destroyed = false;
+	private volatile boolean destroyed = false;
 
 	/** private lock */
 	/**
@@ -364,8 +319,8 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 	 */
 	private final Object lock = new Object();
 
-	/** utility service wrapper */
-	private ServiceWrapper wrapper;
+	/** service reference/service holder */
+	private volatile ReferenceHolder holder;
 
 	/** retry template */
 	private final RetryTemplate retryTemplate = new EventSenderRetryTemplate();
@@ -475,12 +430,10 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 			// set this flag first to make sure no rebind is done
 			destroyed = true;
 			isDuringDestruction = true;
-			if (wrapper != null) {
-				ServiceReference ref = wrapper.getReference();
-				if (ref != null) {
-					// send unregistration event to the listener
-					listener.serviceChanged(new ServiceEvent(ServiceEvent.UNREGISTERING, ref));
-				}
+			if (holder != null) {
+				ServiceReference ref = holder.getReference();
+				// send unregistration event to the listener
+				listener.serviceChanged(new ServiceEvent(ServiceEvent.UNREGISTERING, ref));
 			}
 			/** destruction process has ended */
 			isDuringDestruction = false;
@@ -550,9 +503,8 @@ public class ServiceDynamicInterceptor extends ServiceInvoker implements Initial
 		if (other instanceof ServiceDynamicInterceptor) {
 			ServiceDynamicInterceptor oth = (ServiceDynamicInterceptor) other;
 			return (serviceRequiredAtStartup == oth.serviceRequiredAtStartup
-					&& ObjectUtils.nullSafeEquals(wrapper, oth.wrapper)
-					&& ObjectUtils.nullSafeEquals(filter, oth.filter) && ObjectUtils.nullSafeEquals(retryTemplate,
-					oth.retryTemplate));
+					&& ObjectUtils.nullSafeEquals(holder, oth.holder) && ObjectUtils.nullSafeEquals(filter, oth.filter) && ObjectUtils
+					.nullSafeEquals(retryTemplate, oth.retryTemplate));
 		} else
 			return false;
 	}
